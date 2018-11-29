7597265ea274ff93d37d5a659bbd756847b10706
Dimitri TRIGALOUX <dtrigalo@e1r9p20.42.fr>
2018-11-29 16:54:25 +0100

    init_flag added

--- parse_flag.c
+++ parse_flag.c
@@ -6,12 +6,27 @@
 /*   By: dtrigalo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/29 16:28:38 by dtrigalo          #+#    #+#             */
-/*   Updated: 2018/11/29 16:41:45 by dtrigalo         ###   ########.fr       */
+/*   Updated: 2018/11/29 16:54:00 by dtrigalo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include "libft/libft.h"
+
+t_flag	initialise_flag(void)
+{
+	t_flag	flag[5];
+
+	flag[0].c = '+';
+	flag[1].c = '-';
+	flag[2].c = ' ';
+	flag[3].c = '#';
+	flag[4].c = '0';
+	flag[0].f = &flag_plus;
+	flag[1].f = &flag_minus;
+	flag[2].f = &flag_space;
+	flag[3].f = &flag_hash;
+}
 
 char	*parse_flag(char *str, char *flag)
 {
