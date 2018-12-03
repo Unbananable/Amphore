0a201766f5d3ac7c337d2fb7415f8e2cc0222588
Anaelle LECLABART <anleclab@e3r10p21.42.fr>
2018-12-03 13:43:28 +0100

    added conv_v and conv_c

---
+++ conv_c.c
@@ -0,0 +1,13 @@
+#include "libft/libft.h"
+#include <stdarg.h>
+
+char	*conv_c(va_list ap, char *mod)
+{
+	char			*res;
+	unsigned char	arg;
+
+	arg = (unsigned char)va_arg(ap, int);
+	res = ft_strnew(1);
+	res[0] = arg;
+	return (res);
+}
---
+++ conv_s.c
@@ -0,0 +1,10 @@
+#include "libft/libft.h"
+#include <stdarg.h>
+
+char	*conv_c(va_list ap, char *mod)
+{
+	char	*res;
+
+	res = ft_strdup(va_arg(ap, const char *));
+	return (res);
+}
