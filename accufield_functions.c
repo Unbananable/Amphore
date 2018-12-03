3826b857c261e439e27a69e16015a3169943a940
Anaelle LECLABART <anleclab@e3r10p21.42.fr>
2018-12-03 16:02:43 +0100

    updated accufield functions to account for different behaviour depending on conversion type

--- accufield_functions.c
+++ accufield_functions.c
@@ -6,19 +6,29 @@
 /*   By: anleclab <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/29 17:41:20 by anleclab          #+#    #+#             */
-/*   Updated: 2018/11/30 16:02:09 by anleclab         ###   ########.fr       */
+/*   Updated: 2018/12/03 16:01:58 by anleclab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "libft/libft.h"
 
-char	*accuracy(char *str, int len)
+char	*accuracy(char *str, int len, char c)
 {
 	char	*ret;
 	int		i;
 
-	if (ft_strlen(str) < len)
+	if (c == 'c' || c == 'p')
+		return (str);
+	if (c == 's' && len > ft_strlen(str))
+	{
+		if (!(ret = ft_strnew(len + 1)))
+			return (NULL);
+		ft_strncpy(ret, str, len);
+		free(str);
+		return (ret);
+	}
+	if (c != 'f' && ft_strlen(str) < len)
 	{
 		if (!(ret = (char *)malloc(sizeof(char) * (len + 1))))
 			return (NULL);
@@ -34,7 +44,20 @@ char	*accuracy(char *str, int len)
 		free(str);
 		return (ret);
 	}
-	return (str);
+	i = 0;
+	while (str[i] != '.')
+		i++;
+	if (!(ret = ft_strnew(i + len + 1)))
+		return (NULL);
+	if (i + len > ft_strlen(str))
+	{
+		ft_strncpy(ret, str, ft_strlen(str));
+		ft_memset(ret + ft_strlen(str), i + len - ft_strlen(str), '0');
+		free(str);
+		return (ret);
+	}
+	ft_strncpy(ret, str, i + len);
+	return (ret);
 }
 
 char	*field_width(char *str, int len)
