773313b999895a9972b050f302902cd9353d3d9e
Dimitri TRIGALOUX <dtrigalo@e3r6p19.42.fr>
2018-12-06 19:51:34 +0100

    relief and sadness

--- ft_printf.c
+++ ft_printf.c
@@ -1,9 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dtrigalo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/12/06 19:47:15 by dtrigalo          #+#    #+#             */
+/*   Updated: 2018/12/06 19:47:52 by dtrigalo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft/libft.h"
 #include "ft_printf.h"
 #include <stdlib.h>
 #include <unistd.h>
-
-#include <stdio.h>
 
 static char	*converter(char *str, va_list ap)
 {
@@ -12,51 +22,7 @@ static char	*converter(char *str, va_list ap)
 	res = parse_conv(ap, str);
 	res = parse_accufield(res, str);
 	res = parse_flag(res, str);
-	return(res);
-}
-
-static int	write_and_count(const char *ft, int cnt, int i, va_list ap)
-{
-	char	*str;
-	char	*arg;
-
-	if (!ap)
-	{
-		str = parse_color(ft + i);
-		write(1, ft, i);
-		cnt += i;
-		if (!ft_strequ(str, "\033[0m"))
-			write(1, str, 7);
-		else
-			write(1, str, 4);
-		str = NULL;
-	}
-	else
-	{
-		str = ft_strsub(ft, 0, i + 1);
-		arg = converter(str, ap);
-		write(1, arg, ft_strlen(arg));
-		cnt += ft_strlen(arg);
-		free(str);
-		free(arg);
-	}
-	return (cnt);
-}
-
-static int	write_then_count(const char *format, int i, int count, int mode)
-{
-	if (mode)
-	{
-		write(1, format, i);
-		count += i;
-		return (count);
-	}
-	else
-	{
-		write(1, format, ft_strlen(format));
-		count += ft_strlen(format);
-		return (count);
-	}
+	return (res);
 }
 
 t_form		init_struct(t_form anc, const char *format)
@@ -67,7 +33,7 @@ t_form		init_struct(t_form anc, const char *format)
 	return (anc);
 }
 
-t_form		set_struct(t_form anc)
+t_form		set_struct1(t_form anc)
 {
 	char *str;
 
@@ -84,6 +50,29 @@ t_form		set_struct(t_form anc)
 	return (anc);
 }
 
+t_form		set_struct2(t_form anc, va_list ap)
+{
+	char	*str;
+	char	*arg;
+
+	write(1, anc.fmt, anc.i);
+	anc.cnt += ft_strlen(anc.fmt);
+	anc.fmt += anc.i + 1;
+	anc.i = 0;
+	while (anc.fmt[anc.i] && !ft_strchr("cspdiouxXf%", anc.fmt[anc.i]))
+		anc.i++;
+	if (!anc.fmt[anc.i])
+		exit_error("error: invalid conversion\n", 0);
+	anc.fmt += anc.i + 1;
+	str = ft_strsub(anc.fmt - anc.i - 1, 0, anc.i + 1);
+	arg = converter(str, ap);
+	write(1, arg, ft_strlen(arg));
+	anc.cnt += ft_strlen(arg);
+	free(str);
+	free(arg);
+	return (anc);
+}
+
 int			ft_printf(const char *format, ...)
 {
 	va_list		ap;
@@ -96,28 +85,19 @@ int			ft_printf(const char *format, ...)
 		if (anc.fmt[anc.i] == '{')
 		{
 			if (parse_color(anc.fmt + anc.i))
-				anc = set_struct(anc);
+				anc = set_struct1(anc);
 			else
 				anc.i++;
 		}
 		else if (anc.fmt[anc.i] == '%')
-		if (format[i] == '%')
 		{
-			anc.cnt = write_then_count(anc.fmt, anc.i, anc.cnt, 1);
-			anc.fmt += anc.i + 1;
-			anc.i = 0;
-			while (anc.fmt[anc.i] && !ft_strchr("cspdiouxXf%", anc.fmt[anc.i]))
-				anc.i++;
-			if (!anc.fmt[anc.i])
-				exit_error("error: invalid conversion\n", 0);
-			anc.fmt += anc.i + 1;
-			anc.cnt = write_and_count(anc.fmt - anc.i - 1, anc.cnt, anc.i, ap);
-			anc.i = 0;
+			anc = set_struct2(anc, ap);
 		}
 		else
 			anc.i++;
 	}
-	anc.cnt = write_then_count(anc.fmt, anc.i, anc.cnt, 0);
+	write(1, anc.fmt, ft_strlen(anc.fmt));
+	anc.cnt += ft_strlen(anc.fmt);
 	va_end(ap);
-	return(anc.cnt);
+	return (anc.cnt);
 }
